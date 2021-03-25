18292a3dbcbef67c875ad105e697bfc32092ea50
Carlos Ceagah <carlosceagah@gmail.com>
2021-03-25 18:27:48 -0300

    ADD: baby steps for the win

--- Stage 01 - Fundamentals/lesson01.py
+++ Stage 01 - Fundamentals/lesson01.py
@@ -1,2 +1,13 @@
 print('Vai Carlão !')
 # Print means console.log in Javascript
+
+
+name = 'Carlos'
+age = '27'
+weight = '83'
+
+print(name, age, weight)
+# String concatenation ...
+
+print('Hello , my name is '+ name +' i am '+ age + ' years old, and i have ' + weight + ' kilos')
+# String concatenation with set text ...
