74c5333e65aa51187d8a67543b7b67127772f351
Carlos Ceagah <carlosceagah@gmail.com>
2021-03-25 18:35:30 -0300

    Add: Input Strings , and String concatenations

--- Stage 01 - Fundamentals/lesson01.py
+++ Stage 01 - Fundamentals/lesson01.py
@@ -11,3 +11,11 @@ print(name, age, weight)
 
 print('Hello , my name is '+ name +' i am '+ age + ' years old, and i have ' + weight + ' kilos')
 # String concatenation with set text ...
+
+
+name = input('What is your name? ')
+age = input('How old are you? ')
+
+print('Hello '+ name + " ! . Nice to meet you ! You're a " + age + " years old! I'm 27. You do not ask, but i want to "
+                                                                   "tell you")
+# Input Strings ...
